9496d7026d7f2bdfbb9b2f50c4a24d185162a097
Sohelsh <sohelshaikhadam@gmail.com>
2023-02-07 23:40:55 +0530

    Variable In JS

---
+++ JS/01.Variable.js
@@ -0,0 +1,9 @@
+var No = 21;
+console.log(No);
+
+var Name = "Sohel";
+console.log(Name);
+
+ console.log("Data Type Of First Variable:",typeof(No)); // Datatype Of Variable
+
+console.log("Data Type Of Second Variable:",typeof(Name));
