858ada490a4a460b386a27f77148305ddeb1e007
Sohelsh <sohelshaikhadam@gmail.com>
2023-02-11 22:57:46 +0530

    JS Basic

---
+++ JS/03.Datatype.js
@@ -0,0 +1,21 @@
+// var MyDemo = 10;
+// console.log(typeof(MyDemo));
+
+// var MyNum = BigInt;
+// MyDemo = (14154444454644454);
+// console.log(typeof(MyNum));
+// console.log(MyNum);
+
+// var str = "Hello Word" + "Batch";
+// console.log(typeof(str));
+// console.log(str);
+
+var str = 10 + "20";
+console.log(str);
+
+var str = 9 -"8";
+console.log(str); //Bug In JS
+
+var Arr = [20,30,40];
+console.log(typeof(Arr));
+console.log(Arr);
