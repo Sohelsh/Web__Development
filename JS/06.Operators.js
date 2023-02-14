8f7c69f7450d90bca62d23db8616288b4bd36af0
Sohelsh <sohelshaikhadam@gmail.com>
2023-02-14 23:27:33 +0530

    JS Practice

---
+++ JS/06.Operators.js
@@ -0,0 +1,35 @@
+//Exponentiation Opretors
+
+// var No1 = 10;
+// var No2 = 2;
+
+// console.log(No1 ** No2);
+// console.groupCollapsed(Math.pow(10,2));
+
+//Increment Operators(++)
+ //"Post"
+// var Num1 = 10;
+// console.log(Num1++);
+// console.log(Num1++);
+// //Pre 
+
+// var No = 8;
+// console.log(++No);
+// console.log(++No);
+
+//Decrement (--)
+//post
+// var num1 = 5;
+// console.log(num1--);
+// console.log(num1)
+
+//Pre 
+// var num2 = 9;
+// console.log(--num2);
+
+var N = 5;
+var N = 10;
+
+console.log(N == N); // false
+console.log(N === N); // false
+
