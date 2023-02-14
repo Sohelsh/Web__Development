bb95604dcdca9dcd0d25855336cc4c08c8c3032c
Sohelsh <sohelshaikhadam@gmail.com>
2023-02-15 00:39:00 +0530

    Practice JS

---
+++ JS/07.Statement.js
@@ -0,0 +1,47 @@
+// var No = [10,20,30,40,50];
+// var Sum = 0;
+
+// for(let i = 0;i < No.length;i++)
+// {
+//     Sum+=No[i];
+// }
+// console.log(Sum);
+
+// var color = ['Red','Pick','Yellow','black','white'];
+
+// for(let i = 0;i < color.length;i++)
+// {
+//     if(color[i] === input)
+//     {
+//         console.log('Element Is:',color[i],',Index IS:',i);
+//     }
+// }
+
+
+// for(let No = 2; No <= 10;No++)
+// {
+//     for(let i = 1; i <= 10;i++)
+//     {                                 // Table
+//     console.log(No*i);
+//     }
+//     console.log('\n');
+// }
+
+// var Arr = [10,20,30,40,50];
+// var Sum = 0;
+// for(let i = 0;i < Arr.length;i++)
+// {
+//      Sum += Arr[i];
+// }
+
+//  console.log('Averge Of:',Sum / Arr.length);
+
+var Str = '12345';
+// var newstr = '';
+
+for(var i = Str.length -1; i >= 0;i--)
+{
+    // newstr += (Str[i]);
+    console.log(Str[i]);
+}
+// console.log(newstr);
