15bd865b3a6bd90d5dc736838981feadf11c4e7d
Sohelsh <sohelshaikhadam@gmail.com>
2023-02-14 23:26:46 +0530

    JS Practice

---
+++ JS/04.Array_In_Js.js
@@ -0,0 +1,7 @@
+// const Student = [10,20,30,40,50];
+// Student[Student.length] = 100; 
+// console.log(Student);
+
+let Student = [10,20,30,40,50];
+let LastNum = Student[Student.length-1];
+console.log(LastNum);
