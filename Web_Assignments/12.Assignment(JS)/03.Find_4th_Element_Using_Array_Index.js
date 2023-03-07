f60c58ff9c53edcc6617c903ebed7911a399dd08
Sohelsh <sohelshaikhadam@gmail.com>
2023-03-07 22:18:34 +0530

    Assignment

---
+++ Web_Assignments/12.Assignment(JS)/03.Find_4th_Element_Using_Array_Index.js
@@ -0,0 +1,11 @@
+
+var X = [10,20,true,"Web Development",3.14];
+var Find = 3; // Which Element Find
+
+for(let i = 0; i <= X.length - 1;i++)
+{
+    if(i == Find - 1)
+    {
+        console.log(X[i]);
+    }
+}
