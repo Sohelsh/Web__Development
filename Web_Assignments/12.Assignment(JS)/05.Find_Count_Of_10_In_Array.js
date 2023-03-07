16f54828bd2bd68151f20b561955a732e79e8665
Sohelsh <sohelshaikhadam@gmail.com>
2023-03-07 22:22:26 +0530

    Assignment

---
+++ Web_Assignments/12.Assignment(JS)/05.Find_Count_Of_10_In_Array.js
@@ -0,0 +1,10 @@
+var ArrNum = [21,30,50,70,54,84,78,95,41,25,82,63,36,52,15,78];
+var Cnt  = 1;
+
+for(let i = 0;i <= ArrNum.length - 1;i++)
+{
+    if(i == Cnt - 1)
+    {
+        console.log(ArrNum[i]);
+    }
+}
